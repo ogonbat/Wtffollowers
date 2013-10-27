4bd3c735afcb8f9278c3fd65ea79969862bbf41e
ogonbat <cingusoft@gmail.com>
2013-10-27 06:31:35 +0100

    changed the deploy file and the database in production

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :rvm_ruby_string, 'ruby-2.0.0-p247'
 require 'rvm/capistrano'
 
 #set :repository, "#{user}@#{domain}:/home/#{user}/git/#{application}.git"
-set :repository, "https://cingusoft:nw7a10bt@bitbucket.org/cingusoft/wtffollowers.git"
+set :repository, "https://github.com/ogonbat/Wtffollowers.git"
 set :deploy_to, "/home/#{user}/deploy/#{application}"
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
@@ -25,25 +25,3 @@ set :scm_verbose, true
 set :use_sudo, false
 set :normalize_asset_timestamps, false
 set :rails_env, :production
-
-namespace :deploy do
-  desc "cause Passenger to initiate a restart"
-  task :restart do
-    run "sudo /etc/init.d/nginx stop"
-    run "sudo /etc/init.d/nginx start"
-  end
-end
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
-
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
