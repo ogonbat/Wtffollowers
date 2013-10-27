fe4bc936396611b51d1f4e9ea788cf8457d255b5
ogonbat <cingusoft@gmail.com>
2013-10-27 07:31:52 +0100

    added some check

--- app/controllers/admin/rules_controller.rb
+++ app/controllers/admin/rules_controller.rb
@@ -60,6 +60,7 @@ class Admin::RulesController < ApplicationController
           end
         end
       end
+      puts final_rule
       render json: {result: true, data: final_rule}
     else
       render json: {result: false}
