7d2bf5dd3c80fde022fcfd7be810df0201598500
ogonbat <cingusoft@gmail.com>
2013-10-27 07:38:15 +0100

    some changes

--- app/controllers/admin/scanner_controller.rb
+++ app/controllers/admin/scanner_controller.rb
@@ -1,13 +1,16 @@
 class Admin::ScannerController < ApplicationController
   before_filter :authenticate_user!
   def index
-    my_file = convert_from_base64(params[:params][:photo])
-    resultZbar = ZBar::Image.from_jpeg(my_file).process
-    begin
-      render json: {result: true, code: resultZbar[0].data}
-    rescue NoMethodError => e
-      render json: {result: false}
+    if params[:params][:photo]
+      my_file = convert_from_base64(params[:params][:photo])
+      resultZbar = ZBar::Image.from_jpeg(my_file).process
+      begin
+        render json: {result: true, code: resultZbar[0].data}
+      rescue NoMethodError => e
+        render json: {result: false}
+      end
     end
+    render json: {result: false}
   end
 
   private
